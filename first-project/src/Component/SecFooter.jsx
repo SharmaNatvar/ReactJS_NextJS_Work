7d62e008ebd4a372faba070d95a21031207d700f
Sharma Natvar <121554685+SharmaNatvar@users.noreply.github.com>
2023-10-23 10:26:19 +0530

    Add files via upload

---
+++ first-project/src/Component/SecFooter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+const SecFooter = () =>{
+ return(
+    <>
+        <div className="d-flex">
+            <div className="col-4">
+                <h2>Source</h2>
+                <p>login</p>
+                <p>Term</p>
+                <p>About</p>
+                <p>Help</p>
+            </div>
+            <div className="col-4">
+                <h2>Project</h2>
+                <p>Stock Market</p>
+                <p>textile Market</p>
+                <p>Gold Market</p>
+                <p>Help</p>
+            </div>
+            <div className="col-4">
+                <h2>Product</h2>
+                <p>Excle</p>
+                <p>Account</p>
+                <p>Graph</p>
+                <p>contact</p>
+            </div>
+        </div>
+    </>   
+ )   
+}
+
+export default SecFooter
---
+++ first-project/src/Component/SideFooder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SideFooter = () =>{
+    return (
+        <div>
+            <h1>Finance</h1>
+            <p> </p>
+        </div>
+    )
+}
+
+export default SideFooter
