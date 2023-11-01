d77480d820eac0c0fd460b9c7b1ab3f200ad3fff
Sharma Natvar <121554685+SharmaNatvar@users.noreply.github.com>
2023-11-01 08:13:31 +0530

    Add files via upload

---
+++ cardjson/src/json.js
@@ -0,0 +1,28 @@
+const books = [
+    {
+      book: "Book1",
+      writer: "krishn",
+    },
+    {
+      book: "Book2",
+      writer: "Sharma",
+    },
+    {
+      book: "Book3",
+      writer: "Sharma",
+    },
+    {
+      book: "Book4",
+      writer: "Sharma",
+    },
+    {
+      book: "Book5",
+      writer: "Sharma",
+    },
+    {
+      book: "Book6",
+      writer: "Sharma",
+    },
+  ];
+
+export default books
