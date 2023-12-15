5e2cc1f6852e7bd492626d8fefee6b6194b8a5a1
Sharma Natvar <krish997854@gmail.com>
2023-12-15 21:00:35 +0530

    add curd app

---
+++ curdapp/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Curds from "./Page/Curds";
+
+
+function App() {
+  return (
+    <>
+     <div>
+        <Curds/>
+     </div> 
+    </>
+  );
+}
+
+export default App;
